9c685765d244739d6e6e92f1d6b33f90b84037a1
Soyeon Kim <ese2003@naver.com>
2018-07-23 23:12:16 +0900

    Update 1269.cpp

--- 1269.cpp
+++ 1269.cpp
@@ -1,19 +1,30 @@
 #include <iostream>
 #include <set>
 using namespace std;
+
 set<int> a;
 int n, m;
+
 int main() {
+    // 집합의 원소 개수 입력받음
     scanf(" %d %d", &n, &m);
+    
+    // x 변수는 for문 내부에 선언 후 입력받은 x를 집합에 삽입.
     for (int i = 0; i < n; i++) {
         int x; scanf(" %d", &x);
         a.insert(x);
     }
+    
+    // 중복 제거
+    // 입력받은 x가 집합에 존재하면 (a.count(x) >= 1일 경우) 삭제. -> 입력 받지 않는것과 동일
+    // 존재하지 않으면 삽입. 
     for (int i = 0; i < m; i++) {
         int x; scanf(" %d", &x);
         if (a.count(x)) a.erase(x);
         else a.insert(x);
     }
+    
+    // 중복된 원소가 없는 집합 a의 원소 개수는 즉, 대칭 차집합의 원소 개수. 
     printf("%d\n", a.size());
 }
 
